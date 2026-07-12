b62db0976a734a941020ef6108dc81b3bc25f0b6
jomaia <itzamnamaia@tutamail.com>
2026-07-11 22:50:00 -0300

    primeiro commit

---
+++ cap1/main.py
@@ -0,0 +1,32 @@
+# Exemplo 1: um baralho como uma sequencia de cartas
+#
+
+import collections
+from random import choice
+
+Card = collections.namedtuple("Card", ["rank", "suit"])
+
+
+class FrenchDeck:
+    ranks = [str(n) for n in range(2, 11)] + list("JQKA")
+    suits = "espada copas ouro paus".split()
+
+    def __init__(self):
+        self._cards = [Card(rank, suit) for suit in self.suits for rank in self.ranks]
+
+    def __len__(self):
+        return len(self._cards)
+
+    def __getitem__(self, position):
+        return self._cards[position]
+
+
+deck = FrenchDeck()  # instancia de FrenchDeck
+beer_card = Card("7", "paus")
+print(beer_card)
+print(f"O baralho tem {len(deck)} cartas")
+print(deck[:3])  # usando fatiamento
+print(choice(deck))  # usnado uma função aleatoria
+
+# for carta in sorted(deck):  # iterar cartas e ordenar com sorted
+#   print(carta)
